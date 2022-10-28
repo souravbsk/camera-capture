e0a32ac55e9e8ecbff347ad3abd5ec87e0ad525a
Sourav Basak <110307917+souravbsk@users.noreply.github.com>
2022-10-28 20:45:53 +0600

    app.js

--- app.js
+++ app.js
@@ -13,94 +13,94 @@ https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Taking_still_photos
 var mediaStream = null;
 
 // Prefer camera resolution nearest to 1280x720.
-var constraints = { 
-  audio: false, 
-  video: { 
-    width: {ideal: '100%'}, 
-    height: {ideal: '100vh'},
-    facingMode: "environment"
-  } 
-}; 
-
-async function getMediaStream(constraints) {
-  try {
-    mediaStream =  await navigator.mediaDevices.getUserMedia(constraints);
-    let video = document.getElementById('cam');    
-    video.srcObject = mediaStream;
-    video.onloadedmetadata = (event) => {
-      video.play();
-    };
-  } catch (err)  {    
-    console.error(err.message);   
-  }
+var constraints = {
+    audio: false,
+    video: {
+        width: { ideal: '100%' },
+        height: { ideal: '100vh' },
+        facingMode: "environment"
+    }
 };
 
-async function switchCamera(cameraMode) {  
-  try {
-    // stop the current video stream
-    if (mediaStream != null && mediaStream.active) {
-      var tracks = mediaStream.getVideoTracks();
-      tracks.forEach(track => {
-        track.stop();
-      })      
+async function getMediaStream(constraints) {
+    try {
+        mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+        let video = document.getElementById('cam');
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = (event) => {
+            video.play();
+        };
+    } catch (err) {
+        console.error(err.message);
     }
-    
-    // set the video source to null
-    document.getElementById('cam').srcObject = null;
-    
-    // change "facingMode"
-    constraints.video.facingMode = cameraMode;
-    
-    // get new media stream
-    await getMediaStream(constraints);
-  } catch (err)  {    
-    console.error(err.message); 
-    alert(err.message);
-  }
+};
+
+async function switchCamera(cameraMode) {
+    try {
+        // stop the current video stream
+        if (mediaStream != null && mediaStream.active) {
+            var tracks = mediaStream.getVideoTracks();
+            tracks.forEach(track => {
+                track.stop();
+            })
+        }
+
+        // set the video source to null
+        document.getElementById('cam').srcObject = null;
+
+        // change "facingMode"
+        constraints.video.facingMode = cameraMode;
+
+        // get new media stream
+        await getMediaStream(constraints);
+    } catch (err) {
+        console.error(err.message);
+        alert(err.message);
+    }
 }
 
-function takePicture() {  
-  let canvas = document.getElementById('canvas');
-  let video = document.getElementById('cam');
-  let photo = document.getElementById('photo');  
-  let context = canvas.getContext('2d');
-  
-  const height = video.videoHeight;
-  const width = video.videoWidth;
-  
-  if (width && height) {    
-    canvas.width = width;
-    canvas.height = height;
-    context.drawImage(video, 0, 0, width, height);    
-    var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
-  } else {
-    clearphoto();
-  }
+function takePicture() {
+    let canvas = document.getElementById('canvas');
+    let video = document.getElementById('cam');
+    let photo = document.getElementById('photo');
+    let context = canvas.getContext('2d');
+
+    const height = video.videoHeight;
+    const width = video.videoWidth;
+
+    if (width && height) {
+        canvas.width = width;
+        canvas.height = height;
+        context.drawImage(video, 0, 0, width, height);
+        var data = canvas.toDataURL('image/png');
+        photo.setAttribute('src', data);
+    } else {
+        clearphoto();
+    }
 }
 
 function clearPhoto() {
-  let canvas = document.getElementById('canvas');
-  let photo = document.getElementById('photo');
-  let context = canvas.getContext('2d');
-  
-//   context.fillStyle = "none";
-//   context.fillRect(0, 0, canvas.width, canvas.height);
-  var data = canvas.toDataURL();
-  photo.setAttribute('src', data);
+    let canvas = document.getElementById('canvas');
+    let photo = document.getElementById('photo');
+    let context = canvas.getContext('2d');
+
+    //   context.fillStyle = "none";
+    //   context.fillRect(0, 0, canvas.width, canvas.height);
+    var data = canvas.toDataURL();
+    photo.setAttribute('src', data);
 }
 
 document.getElementById('switchFrontBtn').onclick = (event) => {
-  switchCamera("user");
+    switchCamera("user");
 }
 
-document.getElementById('switchBackBtn').onclick = (event) => {  
-  switchCamera("environment");
+document.getElementById('switchBackBtn').onclick = (event) => {
+    switchCamera("environment");
 }
 
-document.getElementById('snapBtn').onclick = (event) => {  
-  takePicture();
-  event.preventDefault();
+document.getElementById('snapBtn').onclick = (event) => {
+    takePicture();
+    event.preventDefault();
 }
 
 clearPhoto();
